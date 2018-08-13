5d3a31111c29fbb37c7d4ee6de4dd1f8b7a63333
Afulton11 <afulton20@gmail.com>
2018-08-12 19:52:27 -0500

    Added ChangeLog, ChangeLogExclusion, and EventLog database objects.
    * these are used for auditing.
    
    #2

---
+++ Blog.Core/EntityLayer/Dbo/EventLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Blog.Core.EntityLayer.Dbo
+{
+    public class EventLog : IEntity
+    {
+        public Int32? EventLogID { get; set; }
+
+        public Int32? EventType { get; set; }
+
+        public String Key { get; set; }
+
+        public String Message { get; set; }
+
+        public DateTime? EntryDate { get; set; }
+    }
+}
