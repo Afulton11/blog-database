45a61d53b7b8a1b0d8af4616d93c30b186c9b310
Afulton11 <afulton20@gmail.com>
2018-08-12 19:42:36 -0500

    Create the Blog DatabaseContext.
    * Added a Guard Clause package (LiteGuard) to both Blog.Core and Blog projects.
    
    #3

---
+++ Blog.Core/DataLayer/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LiteGuard;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Core.DataLayer
+{
+    public sealed class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options)
+            : base(options)
+        {
+            Guard.AgainstNullArgument(nameof(options), options);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ApplyBlogConfigurations(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void ApplyBlogConfigurations(ModelBuilder modelBuilder)
+        {
+            /* ...Apply mapping all of our POCOs here ... */
+        }
+    }
+}
