e7f2a591120d40e71c157985f792540902e12e2b
Afulton11 <afulton20@gmail.com>
2018-08-12 21:13:09 -0500

    Created a database mapping for the ContentStatus entity.
    * added EntityTypebuilder extensions for Auditable and Concurrent entities.
    
    #3

---
+++ Blog.Core/DataLayer/Mapping/Blog/ContentStatusConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.Core.EntityLayer.Blog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Core.DataLayer.Mapping.Blog
+{
+    public sealed class ContentStatusConfiguration : IEntityTypeConfiguration<ContentStatus>
+    {
+        public void Configure(EntityTypeBuilder<ContentStatus> builder)
+        {
+            // Mapping for table
+            builder.ToTable("ContentStatus", "Blog");
+
+            // set entity key
+            builder.HasKey(p => p.ContentStatusID);
+
+            // mapping for entity columns
+            builder.Property(p => p.ContentStatusName).HasColumnType("varchar(25)").IsRequired();
+        }
+    }
+}
