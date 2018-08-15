4c57ca602eaf501c6054d601056341fcac72530a
Afulton11 <afulton20@gmail.com>
2018-08-14 20:32:15 -0500

    Created Article, Author, and ContentStatus Configurations.
    * Notice, Articles do not have the Next and Previous Articles mapped. These properties will be removed.
    
    #3

--- Blog.Core/DataLayer/Mapping/Blog/ContentStatusConfiguration.cs
+++ Blog.Core/DataLayer/Mapping/Blog/ContentStatusConfiguration.cs
@@ -14,6 +14,9 @@ namespace Blog.Core.DataLayer.Mapping.Blog
             // set entity key
             builder.HasKey(p => p.ContentStatusID);
 
+            // make the id auto increment
+            builder.Property(p => p.ContentStatusID).UseSqlServerIdentityColumn();
+
             // mapping for entity columns
             builder.Property(p => p.ContentStatusName).HasColumnType("varchar(25)").IsRequired();
             builder.ConfigureAuditableEntity();
