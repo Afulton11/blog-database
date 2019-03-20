720af9ae85a009876aea5f91bb71c666cfbfb75e
Andrew Fulton <afulton20@gmail.com>
2019-03-20 17:11:02 -0500

    Add Linq extension for Paging

---
+++ Blog.Core/BusinessLayer/PagingUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Blog.Core.BusinessLayer
+{
+    public static class PagingUtils
+    {
+        public static IQueryable<T> Paging<T>(this IQueryable<T> query, int pageSize, int pageNumber)
+        {
+            return query
+                .Skip(pageSize * pageNumber)
+                .Take(pageNumber);
+        }
+    }
+}
