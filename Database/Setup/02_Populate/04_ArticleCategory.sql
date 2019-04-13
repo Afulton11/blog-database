af241e6e2f0c6f22c6d9c1ff78bd26dd0e96881d
Max McDaniel <max15@ksu.edu>
2019-04-12 22:49:58 -0500

    add files to seed database

---
+++ Database/Setup/02_Populate/04_ArticleCategory.sql
@@ -0,0 +1,11 @@
+﻿USE BlogDatabase;
+GO
+
+INSERT INTO Blog.ArticleCategory ([Name], [CreationUserId])
+VALUES
+	('History', 4),
+	('Lifestyle', 7),
+	('Politics', 67),
+	('Technology', 80),
+	('Music', 5),
+	('Humor', 44);
--- Database/Setup/02_Populate/08_Reason.sql
+++ Database/Setup/02_Populate/08_Reason.sql
@@ -1,13 +1,10 @@
-USE SimpleDatabase;
+﻿USE BlogDatabase;
 GO
 
-INSERT Blog.PointReasons (Reason)
+INSERT Blog.Reason (Reason)
 VALUES
     (N'Published Article'),
     (N'Favorited a comment'),
     (N'Commented on an article'),
     (N'Replied to a new comment thread'),
     (N'Visited website')
-
-SELECT *
-FROM Blog.PointReasons;
