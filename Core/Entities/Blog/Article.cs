5a03e419925b602b8b3d494544bf5a8d8120bc35
Andrew Fulton <afulton20@gmail.com>
2019-04-13 00:17:27 -0500

    Create Article Entity
    Related to #1

---
+++ Core/Entities/Blog/Article.cs
@@ -0,0 +1,18 @@
+﻿using Core.Data;
+using System;
+
+namespace Core.Entities.Blog
+{
+    public class Article : IEntity
+    {
+        public int ArticleID { get; set; }
+        public Author Author { get; set; }
+        public ArticleCategory Category { get; set; }
+        public ContentStatus ContentStatus { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public DateTimeOffset CreationDateTime { get; set; }
+        public DateTimeOffset LastUpdateDateTime { get; set; }
+    }
+}
