f9afa28b9643445d1aa186a9971d37564d066b95
Andrew Fulton <afulton20@gmail.com>
2019-04-13 00:17:18 -0500

    Create ArticleCategory Entity
    Related to #1

---
+++ Core/Entities/Blog/ArticleCategory.cs
@@ -0,0 +1,14 @@
+﻿using Core.Data;
+using System;
+
+namespace Core.Entities.Blog
+{
+    public class ArticleCategory : IEntity
+    {
+        public int ArticleCategoryID { get; set; }
+        public string Name { get; set; }
+        public int CreationUserID { get; set; }
+        public DateTimeOffset CreationDateTime { get; set; }
+        public DateTimeOffset LastUpdateDateTime { get; set; }
+    }
+}
