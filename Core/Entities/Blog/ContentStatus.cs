869584e31f9e448479ace2831222a54fe4fcc689
Andrew Fulton <afulton20@gmail.com>
2019-04-13 00:16:31 -0500

    Create a ContentStatus entity
    Related to #1

---
+++ Core/Entities/Blog/ContentStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Core.Entities.Blog
+{
+    /// <summary>
+    /// An enum-like class that represents the different Content Statuses found within our database.
+    /// This entity contains a limited set of values because it is static within our database.
+    /// </summary>
+    public class ContentStatus
+    {
+        private ContentStatus(string value) { Value = value; }
+        public string Value { get; set; }
+
+        public static ContentStatus Draft => new ContentStatus("Draft");
+        public static ContentStatus Published => new ContentStatus("Published");
+        public static ContentStatus Deleted => new ContentStatus("Deleted");
+        public static ContentStatus Removed => new ContentStatus("Removed");
+        public static ContentStatus Archived => new ContentStatus("Archived");
+
+        public override bool Equals(object obj) =>
+            obj is ContentStatus status
+            && Value == status.Value;
+
+        public override int GetHashCode()=> HashCode.Combine(Value);
+    }
+}
