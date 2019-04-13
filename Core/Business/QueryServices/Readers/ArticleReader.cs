eeb749b1c19bbb3d72e5b6426322a970040aa4ca
Andrew Fulton <afulton20@gmail.com>
2019-04-13 01:43:55 -0500

    [Core] Create an ArticleReader
    * This class will create articles from a data record. Using the nameof on the article properties probably shouldn't be done. The keys will become a literal string shortly.

---
+++ Core/Business/QueryServices/Readers/ArticleReader.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using Core.Business.QueryServices.Readers;
+using Core.Entities.Blog;
+using System;
+using System.Data;
+
+namespace Core.Business.QueryServices.DataReaders
+{
+    /// <summary>
+    /// Reads all articles found in the given <see cref="IDataReader"/>
+    /// </summary>
+    public class ArticleReader : Reader<Article>
+    {
+        protected override Article ReadRow(IDataRecord row) =>
+            new Article
+            {
+                ArticleID = row.GetSafely<int>(nameof(Article.ArticleID)),
+                AuthorID = row.GetSafely<int>(nameof(Article.AuthorID)),
+                CategoryID = row.GetSafely<int>(nameof(Article.CategoryID)),
+                ContentStatus = row.GetSafely<ContentStatus>(nameof(Article.ContentStatus)),
+                Title = row.GetSafely<string>(nameof(Article.Title)),
+                Description = row.GetSafely<string>(nameof(Article.Description)),
+                Body = row.GetSafely<string>(nameof(Article.Body)),
+                CreationDateTime = row.GetSafely<DateTimeOffset>(nameof(Article.CreationDateTime)),
+                LastUpdateDateTime = row.GetSafely<DateTimeOffset>(nameof(Article.LastUpdateDateTime)),
+            };
+    }
+}
