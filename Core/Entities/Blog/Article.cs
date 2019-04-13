da95d7496952e3cf2baccd3890612068d1b00300
Andrew Fulton <afulton20@gmail.com>
2019-04-13 01:10:37 -0500

    [Core] Use ID instead of type for foreign keys
    #1

--- Core/Entities/Blog/Article.cs
+++ Core/Entities/Blog/Article.cs
@@ -6,8 +6,8 @@ namespace Core.Entities.Blog
     public class Article : IEntity
     {
         public int ArticleID { get; set; }
-        public Author Author { get; set; }
-        public ArticleCategory Category { get; set; }
+        public int AuthorID { get; set; }
+        public int CategoryID { get; set; }
         public ContentStatus ContentStatus { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
