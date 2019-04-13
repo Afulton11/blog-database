a04b696bf7d9cfe643f68bbd992bab3d74e3b3b3
carsonholt <cdh1522@gmail.com>
2019-04-12 20:15:35 -0500

    Added Author and Follower classes

---
+++ Core/Entities/Blog/Follower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Core.Data;
+namespace Core.Entities.Blog
+{
+    public class Follower : IEntity
+    {
+        public int FollowedUserID { get; }
+        public int FollowingUserID { get; }
+        public DateTimeOffset AddedAt { get; }
+        public DateTimeOffset DeletedAt { get; }
+    }
+}
