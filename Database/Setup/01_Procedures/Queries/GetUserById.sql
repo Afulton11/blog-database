d75130e8863f6ae211327a04c7af085f6e3c5311
Mackenzie McDaniel <max15@ksu.edu>
2019-04-19 14:00:06 -0500

    adding domain query models

--- Database/Setup/01_Procedures/Queries/GetUserById.sql
+++ Database/Setup/01_Procedures/Queries/GetUserById.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE OR ALTER PROCEDURE Blog.GetUserById
 	@UserId AS INT,
-	@GetDeleted AS BIT = 0
+	@WithDeleted AS BIT = 0
 AS
 	SELECT *
 	FROM Blog.[User] AS u
@@ -12,8 +12,8 @@ AS
 		-- https://csharpcornerplus.wordpress.com/2014/09/24/conditional-where-clause-in-sql-server/
 		AND
 		(
-			@GetDeleted = 1 AND u.DeletedAt IS NOT NULL
+			@WithDeleted = 1 AND u.DeletedAt IS NOT NULL
 			OR
-			@GetDeleted = 0 AND u.DeletedAt IS NULL
+			@WithDeleted = 0 AND u.DeletedAt IS NULL
 		)
 GO
