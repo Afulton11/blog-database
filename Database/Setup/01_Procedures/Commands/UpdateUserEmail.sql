12fd8464cc5fc84786e25668c8a2bcc8a80ed599
Mackenzie McDaniel <max15@ksu.edu>
2019-04-18 22:15:21 -0500

    added basic query and command procedures for user, role, and points

---
+++ Database/Setup/01_Procedures/Commands/UpdateUserEmail.sql
@@ -0,0 +1,14 @@
+﻿USE BlogDatabase;
+GO
+
+-- assumes user with userid exists and no similar (username,email) combinations exist
+CREATE OR ALTER PROCEDURE Blog.UpdateUserEmail
+	@UserId AS INT,
+	@UpdatedEmail AS NVARCHAR(128)
+AS
+	UPDATE Blog.[User]
+		SET
+			Email = @UpdatedEmail,
+			LastUpdatedTime = SYSDATETIME()
+		WHERE UserId = @UserId;
+GO
---
+++ Database/Setup/01_Procedures/Queries/GetUserById.sql
@@ -0,0 +1,10 @@
+﻿USE BlogDatabase;
+GO
+
+CREATE OR ALTER PROCEDURE Blog.GetUserById
+	@UserId AS INT
+AS
+	SELECT *
+	FROM Blog.[User] AS u
+	WHERE u.UserId = @UserId;
+GO
