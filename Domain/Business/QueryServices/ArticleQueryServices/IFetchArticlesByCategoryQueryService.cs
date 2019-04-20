869da0215099ce43fdd5ccf92510c8879514718d
Andrew Fulton <afulton20@gmail.com>
2019-04-20 18:14:28 -0500

    Add FetchArticlesByCategoryQueryService
    #1

---
+++ Domain/Business/QueryServices/ArticleQueryServices/IFetchArticlesByCategoryQueryService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Data.Queries;
+using Domain.Data.Queries.ArticleQueries;
+using Domain.Entities.Blog;
+
+namespace Domain.Business.QueryServices.ArticleQueryServices
+{
+    public interface IFetchArticlesByCategoryQueryService : IQueryService<FetchArticlesByCategoryQuery, Paged<Article>>
+    {
+    }
+}
