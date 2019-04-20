f3c75ef82abb20300f8cf74f2369e3f30d56cb04
Andrew Fulton <afulton20@gmail.com>
2019-04-20 18:38:18 -0500

    [DataAccess] Create FollowerReader

---
+++ DataAccess/DataAccess/QueryServices/Readers/FollowerReader.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using DataAccess.QueryServices.Readers;
+using Domain.Entities.Blog;
+using System;
+using System.Data;
+
+namespace DataAccess.DataAccess.QueryServices.Readers
+{
+    public class FollowerReader : Reader<Follower>
+    {
+        protected override Follower ReadRow(IDataRecord row) =>
+            new Follower
+            {
+                UserId = row.GetSafely<int>(nameof(Follower.UserId)),
+                AddedAt = row.GetSafely<DateTime>(nameof(Follower.AddedAt)),
+                DeletedAt = row.GetSafely<DateTime>(nameof(Follower.DeletedAt)),
+            };
+    }
+}
