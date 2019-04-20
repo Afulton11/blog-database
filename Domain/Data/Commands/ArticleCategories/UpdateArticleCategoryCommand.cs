51c15848a363331c85ef3af69ab2b94e48d8ee56
Andrew Fulton <afulton20@gmail.com>
2019-04-20 17:29:08 -0500

    Add UpdateArticleCategoryService

---
+++ Domain/Data/Commands/ArticleCategories/UpdateArticleCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Blog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Commands.ArticleCategories
+{
+    public class UpdateArticleCategoryCommand : ICommand
+    {
+        /// <summary>
+        /// The <see cref="ArticleCategory"/> to update.
+        /// </summary>
+        [Required]
+        public ArticleCategory ArticleCategory { get; set; }
+    }
+}
