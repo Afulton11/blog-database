dca1ed95760fa351a007da3d6d1ab9585f02eaf5
Andrew Fulton <afulton20@gmail.com>
2019-04-20 18:32:29 -0500

    [DataAcess] Create ArticleCategoryReader

---
+++ DataAccess/DataAccess/QueryServices/Readers/ArticleCategoryReader.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using DataAccess.QueryServices.Readers;
+using Domain.Entities.Blog;
+using System;
+using System.Data;
+
+namespace DataAccess.DataAccess.QueryServices.Readers
+{
+    public class ArticleCategoryReader : Reader<ArticleCategory>
+    {
+        protected override ArticleCategory ReadRow(IDataRecord row) =>
+            new ArticleCategory
+            {
+                ArticleCategoryID = row.GetSafely<int>(nameof(ArticleCategory.ArticleCategoryID)),
+                CreationDateTime = row.GetSafely<DateTime>(nameof(ArticleCategory.CreationDateTime)),
+                CreationUserID = row.GetSafely<int>(nameof(ArticleCategory.CreationUserID)),
+                LastUpdateDateTime = row.GetSafely<DateTime>(nameof(ArticleCategory.LastUpdateDateTime)),
+            };
+    }
+}
