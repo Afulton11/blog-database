ecfadd0486b1b6e2e9b05bb139d3f8676ff0424a
Andrew Fulton <afulton20@gmail.com>
2019-04-19 20:51:38 -0500

    [Common] Add an 'AddAll' extension for a IDataParameterCollection.

---
+++ Common/DataParameterCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using EnsureThat;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Common
+{
+    public static class DataParameterCollectionExtensions
+    {
+        public static void AddAll(this IDataParameterCollection collection, params IDataParameter[] parameters) =>
+            AddAll(collection, parameters);
+
+        public static void AddAll(this IDataParameterCollection collection, IEnumerable<IDataParameter> parameters)
+        {
+            EnsureArg.IsNotNull(collection, nameof(collection));
+
+
+            foreach (var p in parameters)
+            {
+                collection.AddAll(p);
+            }
+        }
+    }
+}
