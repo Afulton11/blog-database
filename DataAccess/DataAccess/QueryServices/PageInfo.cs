d1d1a93d8ac1c481b9d9fd9f27d5cc1914d8960a
Andrew Fulton <afulton20@gmail.com>
2019-04-20 14:01:43 -0500

    Add FetchUserFavoriteArticlesQuery

---
+++ DataAccess/DataAccess/QueryServices/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace DataAccess.DataAccess.QueryServices
+{
+    public class PageInfo
+    {
+        /// <summary>
+        /// The 0-based page index.
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// The number of items in a single page.
+        /// </summary>
+        public int PageSize { get; set; } = 15;
+    }
+}
---
+++ Domain/Data/Queries/ArticleQueries/FetchUserFavoriteArticlesQuery.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.DataAccess.QueryServices;
+using Domain.Entities.Blog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Queries
+{
+    /// <summary>
+    /// A Command used to fetch all articles a user has favorited
+    /// We say "fetch" because this query does not throw if no articles are found, rather it returns null.
+    /// </summary>
+    public class FetchUserFavoriteArticlesQuery : IQuery<Paged<Article>>
+    {
+        /// <summary>
+        /// The user to retrieve a list of favorites for.
+        /// </summary>
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public PageInfo Paging { get; set; }
+    }
+}
