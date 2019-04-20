cbe45516d53e1d537e44349cf95085e9b5a3eddb
Andrew Fulton <afulton20@gmail.com>
2019-04-20 16:46:42 -0500

    Add RemoveArticleCommandService

---
+++ Domain/Data/Commands/Articles/RemoveArticleCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Commands.Articles
+{
+    public class RemoveArticleCommand : ICommand
+    {
+        /// <summary>
+        /// The <see cref="ArticleId"/> of the the <see cref="Entities.Blog.Article"/> to remove.
+        /// </summary>
+        [Required]
+        public int ArticleId { get; set; }
+    }
+}
