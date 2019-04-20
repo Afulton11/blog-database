d877aa8403ebf1b0b06227b1f9dc45946a35d3c5
Andrew Fulton <afulton20@gmail.com>
2019-04-20 15:56:37 -0500

    Add FetchRecentArticlesQueryService
    #1

---
+++ Domain/Data/Queries/ArticleQueries/FetchRecentArticlesQuery.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.DataAccess.QueryServices;
+using Domain.Entities.Blog;
+
+namespace Domain.Data.Queries.ArticleQueries
+{
+    public class FetchRecentArticlesQuery : IQuery<Paged<Article>>
+    {
+        public PageInfo Paging { get; set; } = new PageInfo();
+    }
+}
