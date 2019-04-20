ab0cb0bd444d90b4d084a0dd44cb8fbb84feca41
Andrew Fulton <afulton20@gmail.com>
2019-04-20 14:56:50 -0500

    Add FetchUserCommentsQuery
    #1

---
+++ Domain/Data/Queries/CommentQueries/FetchUserCommentsQuery.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.DataAccess.QueryServices;
+using Domain.Entities.Blog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Queries.CommentQueries
+{
+    public class FetchUserCommentsQuery : IQuery<Paged<Comment>>
+    {
+        /// <summary>
+        /// The <see cref="User.UserId"/> to fetch comments for.
+        /// </summary>
+        [Required]
+        public int UserId { get; set; }
+        public PageInfo Paging { get; set; } = new PageInfo();
+    }
+}
