6f30283c2828a14612e84e9bf6cae6e68faca23f
Andrew Fulton <afulton20@gmail.com>
2019-04-20 15:10:25 -0500

    Add FetchArticleCommentsQueryService

---
+++ Domain/Data/Queries/CommentQueries/FetchArticleCommentsQuery.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.DataAccess.QueryServices;
+using Domain.Entities.Blog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Queries.CommentQueries
+{
+    public class FetchArticleCommentsQuery : IQuery<Paged<Comment>>
+    {
+        /// <summary>
+        /// The <see cref="Article.ArticleId"/> to fetch comments for.
+        /// </summary>
+        [Required]
+        public int ArticleId { get; set; }
+        public PageInfo Paging { get; set; } = new PageInfo();
+    }
+}
