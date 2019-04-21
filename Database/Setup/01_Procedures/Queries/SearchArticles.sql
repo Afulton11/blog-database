3bdd873632e1861e3d05c8f54b274e7aa7bb0023
Andrew Fulton <afulton20@gmail.com>
2019-04-21 18:13:18 -0500

    Add FetchArticlesBySearchQuery

---
+++ Database/Setup/01_Procedures/Queries/SearchArticles.sql
@@ -0,0 +1,27 @@
+﻿USE BlogDatabase;
+GO
+
+
+CREATE OR ALTER PROCEDURE Blog.SearchArticles
+(
+	@SearchText NVARCHAR(510),
+	@PageNumber INT = 0,
+	@PageSize INT = 10
+)
+AS
+BEGIN
+	DECLARE @Search NVARCHAR(512) = '%' + @SearchText + '%'
+
+	SELECT *
+	FROM Blog.Article A
+	WHERE A.DeletedAt IS NULL
+		AND (
+			A.Body LIKE @Search
+			OR A.Title LIKE @Search
+			OR A.[Description] LIKE @Search
+		)
+	ORDER BY A.CreationDateTime DESC
+	OFFSET @PageNumber * @PageSize ROWS
+	FETCH NEXT @PageNumber ROWS ONLY
+END
+GO
---
+++ Domain/Data/Queries/ArticleQueries/FetchArticlesBySearchQuery.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Blog;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Data.Queries.ArticleQueries
+{
+    public class FetchArticlesBySearchQuery : IPaginatedQuery<Article>
+    {
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(510)]
+        public string SearchText { get; set; }
+        public PageInfo Paging { get; set; } = new PageInfo();
+    }
+}
