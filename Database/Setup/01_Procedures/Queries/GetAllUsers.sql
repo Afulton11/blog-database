f48cd7084cbedc0521cfdba339be9146cf280003
Max McDaniel <max15@ksu.edu>
2019-04-22 13:09:38 -0500

    fixed typo and organization updates

--- Database/Setup/01_Procedures/Queries/GetAllUsers.sql
+++ Database/Setup/01_Procedures/Queries/GetAllUsers.sql
@@ -2,7 +2,9 @@
 GO
 
 CREATE OR ALTER PROCEDURE Blog.GetAllUsers
-	@WithDeleted AS BIT = 0
+	@WithDeleted AS BIT = 0,
+	@PageSize AS INT = 10,
+	@PageNumber AS INT = 0
 AS
 	SELECT *
 	FROM Blog.[User] AS u
@@ -12,4 +14,7 @@ AS
 		OR
 		@WithDeleted = 1 AND (u.DeletedAt IS NULL OR u.DeletedAt IS NOT NULL)
 	)
+	ORDER BY u.UserId ASC
+	OFFSET @PageSize * @PageNumber ROWS
+	FETCH NEXT @PageSize ROWS ONLY;
 GO
