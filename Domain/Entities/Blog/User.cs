511fd19bbdb39746e135bf6f44c8875e2a13f1d3
Andrew Fulton <afulton20@gmail.com>
2019-04-22 01:38:52 -0500

    [Domain] Remove 'Salt' From User dto

--- Domain/Entities/Blog/User.cs
+++ Domain/Entities/Blog/User.cs
@@ -9,7 +9,6 @@ namespace Domain.Entities.Blog
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Salt { get; set; }
         public bool IsEmailVerified { get; set; }
         public DateTime CreationDateTime { get; set; }
         public DateTime LastUpdatedDateTime { get; set; }
