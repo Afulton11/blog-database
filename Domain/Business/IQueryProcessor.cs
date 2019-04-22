a089bd85aae1b75ba4c25dbf02afc914d1a2a88a
Andrew Fulton <afulton20@gmail.com>
2019-04-21 22:35:20 -0500

    [Domain] Add IQueryProcessor

---
+++ Domain/Business/IQueryProcessor.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Data.Queries;
+using System.Threading.Tasks;
+
+namespace Domain.Business
+{
+    public interface IQueryProcessor
+    {
+        Task<TResult> Execute<TResult>(IQuery<TResult> query);
+    }
+}
