f5482dc39b916f2e51bd7c4ae80d34c9d8445a7f
Andrew Fulton <afulton20@gmail.com>
2019-04-21 22:35:03 -0500

    [Domain] add an Asynchronous query processor
    * This allows us to simply use the Query POCO we created and execute the query service for that query. This means we only need the following classes now to create a query.
    
      - A concrete Implementation of IQueryService<TQuery, TResult>
     - A concrete TQuery<TResult> POCO

---
+++ Domain/Business/DynamicAsyncQueryProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Domain.Business.QueryServices;
+using Domain.Data.Queries;
+using EnsureThat;
+using SimpleInjector;
+
+namespace Domain.Business
+{
+    public class DynamicAsyncQueryProcessor : IQueryProcessor
+    {
+        private readonly Container container;
+
+        public DynamicAsyncQueryProcessor(Container container)
+        {
+            EnsureArg.IsNotNull(container, nameof(container));
+
+            this.container = container;
+        }
+
+        public async Task<TResult> Execute<TResult>(IQuery<TResult> query)
+        {
+            var serviceType = typeof(IQueryService<,>).MakeGenericType(query.GetType(), typeof(TResult));
+
+            dynamic service = this.container.GetInstance(serviceType);
+
+            return await Task.Factory.StartNew(() =>
+            {
+                return service.Execute((dynamic)query);
+            });
+        }
+    }
+}
