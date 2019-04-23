8c3393482de55187f66122e8ff0e4bb967e1b343
Andrew Fulton <afulton20@gmail.com>
2019-04-23 04:28:32 -0500

    Add AuthorReader

---
+++ DataAccess/DataAccess/QueryServices/Readers/AuthorReader.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using DataAccess.QueryServices.Readers;
+using Domain.Entities.Blog;
+using System;
+using System.Data;
+
+namespace DataAccess.DataAccess.QueryServices.Readers
+{
+    public class AuthorReader : Reader<Author>
+    {
+        protected override Author ReadRow(IDataRecord row) =>
+            new Author
+            {
+                AuthorUserId = row.GetSafely<int>(nameof(Author.AuthorUserId)),
+                FirstName = row.GetSafely<string>(nameof(Author.FirstName)),
+                MiddleName = row.GetSafely<string>(nameof(Author.MiddleName)),
+                LastName = row.GetSafely<string>(nameof(Author.LastName)),
+                BirthDate = row.GetSafely<DateTime>(nameof(Author.BirthDate)),
+                DeletedAt = row.GetSafely<DateTime>(nameof(Author.DeletedAt)),
+            };
+    }
+}
--- Domain/Entities/Blog/Author.cs
+++ Domain/Entities/Blog/Author.cs
@@ -4,11 +4,11 @@ namespace Domain.Entities.Blog
 {
     public class Author : IEntity
     {
-        public int AuthorUserID { get; }
+        public int AuthorUserId { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public DateTimeOffset BirthDate { get; set; }
-        public DateTimeOffset DeletedAt { get; }
+        public DateTimeOffset DeletedAt { get; set; }
     }
 }
