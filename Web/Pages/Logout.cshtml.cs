fac1c640f3e1900e899f592e2145d0e69b2e9371
Andrew Fulton <afulton20@gmail.com>
2019-04-23 04:00:39 -0500

    [Web] Add ability to logout

---
+++ Web/Pages/Logout.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Domain.Entities.Blog;
+using EnsureThat;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private SignInManager<User> signInManager;
+
+        public LogoutModel(
+            SignInManager<User> signInManager)
+        {
+            EnsureArg.IsNotNull(signInManager, nameof(signInManager));
+
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            await signInManager.SignOutAsync();
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            return Page();
+        }
+    }
+}
