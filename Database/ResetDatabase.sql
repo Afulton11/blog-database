f0c7b410062ba440080a2591229b59246ae0e94e
Andrew Fulton <afulton20@gmail.com>
2019-04-23 19:58:25 -0500

    [Database] Add query to FetchArticleComments

--- Database/ResetDatabase.sql
+++ Database/ResetDatabase.sql
@@ -37,6 +37,7 @@
 :r ./Setup/01_Procedures/Queries/FetchRoleByNormalizedName.sql
 :r ./Setup/01_Procedures/Queries/FetchAuthorById.sql
 :r ./Setup/01_Procedures/Queries/FetchArticleCategories.sql
+:r ./Setup/01_Procedures/Queries/FetchArticleComments.sql
 
 -- Populate Tables
 :r ./Setup/02_Populate/00_Role.sql
---
+++ Database/Setup/01_Procedures/Queries/FetchArticleComments.sql
@@ -0,0 +1,76 @@
+﻿USE BlogDatabase
+GO
+
+
+CREATE OR ALTER FUNCTION Blog.FetchAllComments()
+RETURNS TABLE
+AS
+RETURN(
+	SELECT 
+		*,
+		CASE WHEN Com.ParentCommentId IS NOT NULL 
+			THEN
+			ROW_NUMBER() OVER(
+				PARTITION BY Com.ParentCommentId
+				ORDER BY Com.CreationDateTime ASC
+			)
+		ELSE
+			0
+		END AS ReplyNumber
+		FROM Blog.Comment Com
+)
+GO
+
+CREATE OR ALTER FUNCTION Blog.FetchAllArticleComments
+(
+	@ArticleId INT,
+	@MaximumRepliesToFetch INT = 3
+)
+RETURNS TABLE 
+AS
+RETURN (
+	SELECT
+		*,
+		CASE WHEN Com.ReplyNumber = 0
+		THEN
+			ROW_NUMBER() OVER(
+				PARTITION BY Com.ReplyNumber
+				ORDER BY Com.CreationDateTime ASC
+			)
+		ELSE
+			0
+		END AS CommentNumber
+	FROM Blog.FetchAllComments() Com
+	WHERE Com.ArticleId = @ArticleId AND Com.ReplyNumber <= @MaximumRepliesToFetch
+)
+GO
+
+/**
+	Returns a paginated list of comments for an article Along with the
+	first @MaximumRepliesToFetch replies for each comment.
+
+	The @PageSize for this query doesn't determine the amount of results unless @MaximumRepliesToFetch = 0.
+*/
+CREATE OR ALTER PROCEDURE Blog.FetchArticleComments
+	@ArticleId INT,
+	@MaximumRepliesToFetch INT = 3,
+	@PageSize INT = 10,
+	@PageNumber INT = 0
+AS
+BEGIN
+	SELECT
+		Com.CommentId,
+		Com.ParentCommentId,
+		Com.UserId,
+		U.Username,
+		Com.CreationDateTime,
+		Com.Body,
+		Com.DeletedAt
+	FROM Blog.FetchAllArticleComments(@ArticleId, @MaximumRepliesToFetch) Com
+		INNER JOIN Blog.[User] U ON U.UserId = Com.UserId
+	WHERE (Com.CommentNumber > @PageSize * @PageNumber
+		   AND Com.CommentNumber <= @PageSize * @PageNumber + @PageSize )
+		  OR Com.CommentNumber = 0
+	ORDER BY CreationDateTime ASC, U.Username DESC
+END
+GO
