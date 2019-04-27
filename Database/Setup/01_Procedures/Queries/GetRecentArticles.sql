d7366ab23ce882b3875e72dcb33c00a4a7ee5e37
Andrew Fulton <afulton20@gmail.com>
2019-04-27 16:17:51 -0500

    [Database] Filter to only published articles for recent articles

--- Database/Setup/01_Procedures/Queries/GetRecentArticles.sql
+++ Database/Setup/01_Procedures/Queries/GetRecentArticles.sql
@@ -12,8 +12,11 @@ BEGIN
 		   Art.AuthorID,
 	       Art.Title,
 	       Art.[Description],
-	       Art.CreationDateTime
+	       Art.CreationDateTime,
+		   C.[Name] AS ContentStatus
 	FROM Blog.Article Art
+		INNER JOIN Blog.ContentStatus C ON C.ContentStatusId = C.ContentStatusId
+	WHERE Art.DeletedAt IS NULL AND C.[Name] = N'Published'
 	ORDER BY Art.CreationDateTime DESC
 	OFFSET @PageSize * @PageNumber ROWS
 	FETCH NEXT @PageSize ROWS ONLY;
