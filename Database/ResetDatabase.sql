4a8bacb67bb1c4f88364787dafd1421056bf91b1
Andrew Fulton <afulton20@gmail.com>
2019-04-29 21:38:48 -0500

    [Database] Rename folders and files
    * Renamed to fit the project-submission requirements

--- Database/ResetDatabase.sql
+++ Database/ResetDatabase.sql
@@ -1,65 +1,66 @@
 ﻿-- Enable SQLCMD Mode.
 
 -- remove and create Tables
-:r ./Setup/00_Blog.sql
+:r ./Setup/Tables.sql
 
-:r ./Setup/triggers.sql
+:r ./Setup/Triggers.sql
 
 -- Create OR Alter Command Procedures
-:r ./Setup/01_Procedures/Commands/CreatePoints.sql
-:r ./Setup/01_Procedures/Commands/CreateUser.sql
-:r ./Setup/01_Procedures/Commands/DeleteUser.sql
-:r ./Setup/01_Procedures/Commands/UpdateUserEmail.sql
-:r ./Setup/01_Procedures/Commands/UpdateUsername.sql
-:r ./Setup/01_Procedures/Commands/VerifyUserEmail.sql
-:r ./Setup/01_Procedures/Commands/CreateOrUpdateRole.sql
-:r ./Setup/01_Procedures/Commands/CreateOrUpdateArticle.sql
-:r ./Setup/01_Procedures/Commands/CreateOrUpdateComment.sql
-:r ./Setup/01_Procedures/Commands/CreateOrUpdateAuthor.sql
-:r ./Setup/01_Procedures/Commands/DeleteRole.sql
-:r ./Setup/01_Procedures/Commands/CreateFavorite.sql
-:r ./Setup/01_Procedures/Commands/DeleteFavorite.sql
+:r ./Setup/Procedures/Commands/CreatePoints.sql
+:r ./Setup/Procedures/Commands/CreateUser.sql
+:r ./Setup/Procedures/Commands/DeleteUser.sql
+:r ./Setup/Procedures/Commands/UpdateUserEmail.sql
+:r ./Setup/Procedures/Commands/UpdateUsername.sql
+:r ./Setup/Procedures/Commands/VerifyUserEmail.sql
+:r ./Setup/Procedures/Commands/CreateOrUpdateRole.sql
+:r ./Setup/Procedures/Commands/CreateOrUpdateArticle.sql
+:r ./Setup/Procedures/Commands/CreateOrUpdateComment.sql
+:r ./Setup/Procedures/Commands/CreateOrUpdateAuthor.sql
+:r ./Setup/Procedures/Commands/DeleteRole.sql
+:r ./Setup/Procedures/Commands/CreateFavorite.sql
+:r ./Setup/Procedures/Commands/DeleteFavorite.sql
 
 -- Create OR Alter Query Procedures
-:r ./Setup/01_Procedures/Queries/GetArticleById.sql
-:r ./Setup/01_Procedures/Queries/GetTotalPointsByUserId.sql
-:r ./Setup/01_Procedures/Queries/GetArticleCategoryById.sql
-:r ./Setup/01_Procedures/Queries/GetArticlesInCategory.sql
-:r ./Setup/01_Procedures/Queries/GetArticlesInCategory.sql
-:r ./Setup/01_Procedures/Queries/FetchArticlesByAuthorId.sql
-:r ./Setup/01_Procedures/Queries/FetchUserByUsername.sql
-:r ./Setup/01_Procedures/Queries/FetchUserByNormalizedUsername.sql
-:r ./Setup/01_Procedures/Queries/FetchUserByNormalizedEmail.sql
-:r ./Setup/01_Procedures/Queries/GetAllUsers.sql
-:r ./Setup/01_Procedures/Queries/GetUserById.sql
-:r ./Setup/01_Procedures/Queries/GetUserRoles.sql
-:r ./Setup/01_Procedures/Queries/GetReasonForPoint.sql
-:r ./Setup/01_Procedures/Queries/GetTotalPointsByUserId.sql
-:r ./Setup/01_Procedures/Queries/GetRecentArticles.sql
-:r ./Setup/01_Procedures/Queries/SearchArticles.sql
-:r ./Setup/01_Procedures/Queries/FetchRoleById.sql
-:r ./Setup/01_Procedures/Queries/FetchRoleByNormalizedName.sql
-:r ./Setup/01_Procedures/Queries/FetchAuthorById.sql
-:r ./Setup/01_Procedures/Queries/FetchArticleCategories.sql
-:r ./Setup/01_Procedures/Queries/FetchArticleComments.sql
+:r ./Setup/Procedures/Queries/GetArticleById.sql
+:r ./Setup/Procedures/Queries/GetTotalPointsByUserId.sql
+:r ./Setup/Procedures/Queries/GetArticleCategoryById.sql
+:r ./Setup/Procedures/Queries/GetArticlesInCategory.sql
+:r ./Setup/Procedures/Queries/GetArticlesInCategory.sql
+:r ./Setup/Procedures/Queries/FetchArticlesByAuthorId.sql
+:r ./Setup/Procedures/Queries/FetchUserByUsername.sql
+:r ./Setup/Procedures/Queries/FetchUserByNormalizedUsername.sql
+:r ./Setup/Procedures/Queries/FetchUserByNormalizedEmail.sql
+:r ./Setup/Procedures/Queries/GetAllUsers.sql
+:r ./Setup/Procedures/Queries/GetUserById.sql
+:r ./Setup/Procedures/Queries/GetUserRoles.sql
+:r ./Setup/Procedures/Queries/GetReasonForPoint.sql
+:r ./Setup/Procedures/Queries/GetTotalPointsByUserId.sql
+:r ./Setup/Procedures/Queries/GetRecentArticles.sql
+:r ./Setup/Procedures/Queries/SearchArticles.sql
+:r ./Setup/Procedures/Queries/FetchRoleById.sql
+:r ./Setup/Procedures/Queries/FetchRoleByNormalizedName.sql
+:r ./Setup/Procedures/Queries/FetchAuthorById.sql
+:r ./Setup/Procedures/Queries/FetchArticleCategories.sql
+:r ./Setup/Procedures/Queries/FetchArticleComments.sql
 
-:r ./Setup/01_Procedures/Queries/FetchPointsByUserId.sql
-:r ./Setup/01_Procedures/Queries/FetchAllPointReasons.sql
-:r ./Setup/01_Procedures/Queries/GetPointBreakdownByUserId.sql
-:r ./Setup/01_Procedures/Queries/FetchArticleFavorites.sql
-:r ./Setup/01_Procedures/Queries/FetchArticlePageById.sql
-:r ./Setup/01_Procedures/Queries/FetchFavoriteArticlesByUserId.sql
+:r ./Setup/Procedures/Queries/FetchPointsByUserId.sql
+:r ./Setup/Procedures/Queries/FetchAllPointReasons.sql
+:r ./Setup/Procedures/Queries/GetPointBreakdownByUserId.sql
+:r ./Setup/Procedures/Queries/FetchArticleFavorites.sql
+:r ./Setup/Procedures/Queries/FetchArticlePageById.sql
+:r ./Setup/Procedures/Queries/FetchFavoriteArticlesByUserId.sql
 
 -- Populate Tables
-:r ./Setup/02_Populate/00_Role.sql
-:r ./Setup/02_Populate/01_User.sql
-:r ./Setup/02_Populate/02_Author.sql
-:r ./Setup/02_Populate/03_ContentStatus.sql
-:r ./Setup/02_Populate/04_ArticleCategory.sql
-:r ./Setup/02_Populate/05_Article.sql
-:r ./Setup/02_Populate/06_Favorite.sql
-:r ./Setup/02_Populate/07_Comment.sql
-:r ./Setup/02_Populate/07_Comment2.sql
-:r ./Setup/02_Populate/07_Comment3.sql
-:r ./Setup/02_Populate/08_Reason.sql
-:r ./Setup/02_Populate/09_Point.sql
+:r ./Setup/Data/00_Role.sql
+:r ./Setup/Data/01_User.sql
+:r ./Setup/Data/02_Author.sql
+:r ./Setup/Data/03_ContentStatus.sql
+:r ./Setup/Data/04_ArticleCategory.sql
+:r ./Setup/Data/05_Article.sql
+:r ./Setup/Data/06_Favorite.sql
+:r ./Setup/Data/07_Comment.sql
+:r ./Setup/Data/07_Comment2.sql
+:r ./Setup/Data/07_Comment3.sql
+:r ./Setup/Data/07_Comment4.sql
+:r ./Setup/Data/08_Reason.sql
+:r ./Setup/Data/09_Point.sql
